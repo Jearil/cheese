23432af781a0f98b403e46dade973acc2d41b830
Colin Miller <jearil@gmail.com>
2012-08-20 19:08:29 -0700

    Moved pins and fixed bugs
    Moved some pins around to avoid using pin 0, 1, and 13. Ended up having
    to remove the humidity controls to get back those pins for now.
    
    Made changes to how the cool down for the freezer works to use unsigned
    longs. Before I had an int which made it not work correctly. Added in a
    first_use variable so that the coil delay was ignored the first time the
    program is run (when it's turned on).
    
    Also made changes to deal with millis() rollover. This version is the
    version that will go into the initial run of the cheese cave.

--- cheese.ino
+++ cheese.ino
@@ -6,17 +6,17 @@
   LCD: https://github.com/ladyada/Adafruit_CharacterOLED
   
   LCD circuit:
-  * LCD RS pin to digital pin 6
-  * LCD R/W pin to digital pin 7
-  * LCD Enable pin to digital pin 8
-  * LCD D4 pin to digital pin 9
-  * LCD D5 pin to digital pin 10
-  * LCD D6 pin to digital pin 11
-  * LCD D7 pin to digital pin 12
+  * LCD RS pin to digital pin 5
+  * LCD R/W pin to digital pin 6
+  * LCD Enable pin to digital pin 7
+  * LCD D4 pin to digital pin 8
+  * LCD D5 pin to digital pin 9
+  * LCD D6 pin to digital pin 10
+  * LCD D7 pin to digital pin 11
 
   DHT22 (Humidity/Temperature) circuit:
   * DHT22 pin 1 to +5V
-  * DHT22 pin 2 to digital pin 2
+  * DHT22 pin 2 to digital pin 4
   * DHT22 pin 4 to GROUND
   * 10K resistor from pin 2 to pin 1 of sensor
 
@@ -24,27 +24,27 @@
  */
 #include <stdio.h>
 #include <Adafruit_CharacterOLED.h>
-#include <DHT.h>
+#include <DHT.h>``
 
-#define DHTPIN 5 // Using pin 5 for DHT
+#define DHTPIN 4 // Using pin 5 for DHT
 #define DHTTYPE DHT22 // Using a DHT22
 
-#define COIL_MIN_REST 15 * 60 * 1000
+unsigned long COIL_MIN_REST = 600000l;
 
 // Buttons
-#define TEMP_UP_PIN 1
-#define TEMP_DOWN_PIN 2
-#define HUMID_UP_PIN 3
-#define HUMID_DOWN_PIN 4
+#define TEMP_UP_PIN 2
+#define TEMP_DOWN_PIN 3
+#define HUMID_UP_PIN 0
+#define HUMID_DOWN_PIN 0
 
 // Freezer power
-#define COOLING_PIN 13
+#define COOLING_PIN 12
 
-Adafruit_CharacterOLED lcd(6, 7, 8, 9, 10, 11, 12);
+Adafruit_CharacterOLED lcd(5, 6, 7, 8, 9, 10, 11);
 DHT dht(DHTPIN, DHTTYPE);
 
 // Length of time we want any custom text to stay for.
-int text_hang_time = 10000;
+unsigned long text_hang_time = 10000;
 unsigned long custom_text_time;
 unsigned long alternate_time_on;
 boolean display_temp;
@@ -52,17 +52,25 @@ boolean display_temp;
 float target_temp;
 float target_humid;
 
+boolean first_run;
+
 // 15 minutes min wait for cooldown
 unsigned long cooling_off_time;
 float temp_band;
 
 boolean cooling;
+unsigned long rollovers; // number of rollovers
+unsigned long start_time;
 
 void setup()
 {
+    first_run = true;
     Serial.begin(9600);
+    Serial.println("Cheese-O-Matic");
     // LCD
+    Serial.println("Starting LCD");
     lcd.begin(16,2);
+    Serial.println("LCD Set");
     lcd.print("Cheese-O-Matic");
     lcd.setCursor(0, 1);
     lcd.print("8000");
@@ -83,14 +91,23 @@ void setup()
     cooling = false;
     custom_text_time = millis();
     alternate_time_on = millis();
+    start_time = millis();
     display_temp = true;
+    Serial.println("Finished Setup");
 }
 
 void loop()
 {
     Serial.println("Starting loop");
-    // Add some profiling
-    unsigned long start_time = millis();
+    // check for rollover
+    unsigned long last_start = start_time;
+    start_time = millis();
+    if (start_time < last_start) {
+      cooling_off_time = start_time;
+      custom_text_time = start_time;
+      alternate_time_on = start_time;
+      rollovers++;
+    }
     
     // Check temp/humidity
     // True for F
@@ -115,6 +132,8 @@ void loop()
     
     // Clear custom text
     clear_custom_text();
+    Serial.print("Target temp: ");
+    Serial.println(target_temp);
 }
 
 void print_stats(float temp, float humid, unsigned long duration)
@@ -148,6 +167,7 @@ void check_buttons()
     int humid_up_button = digitalRead(HUMID_UP_PIN);
     int humid_down_button = digitalRead(HUMID_DOWN_PIN);
     if (temp_up_button == HIGH) {
+        Serial.println("Raising temp");
         target_temp++;
         display_target_temp();
         custom_text_time = millis();
@@ -167,6 +187,7 @@ void adjust_temp(float temp)
 {
     Serial.println("Adjusting temp");
    if(cooling) {
+       Serial.println("Still cooling");
        cooling_shutoff(temp);
    } else {
        float target_high = target_temp + temp_band;
@@ -182,13 +203,26 @@ void adjust_temp(float temp)
 */
 void start_cooling()
 {
-    int time_passed = millis() - cooling_off_time;
-    if (time_passed >= COIL_MIN_REST) {
+    unsigned long time_passed = millis() - cooling_off_time;
+    if (time_passed >= COIL_MIN_REST || first_run) {
+        first_run = false;
         // Turn freezer pin on
         digitalWrite(COOLING_PIN, HIGH);
         cooling = true;
         lcd.print(">>Now Cooling<<");
         custom_text_time = millis();
+        Serial.println("Starting cooldown");
+    } else {
+       unsigned long remaining = COIL_MIN_REST - time_passed;
+       Serial.println("Can't cool, Coils resting");
+       Serial.print("Time Remaining: ");
+       Serial.println(remaining);
+       Serial.print("Time passed: ");
+       Serial.println(time_passed);
+       Serial.print("COIL_MIN_REST: ");
+       Serial.println(COIL_MIN_REST);
+       Serial.print("Cooling off time: ");
+       Serial.println(cooling_off_time);
     }
 }
 
@@ -198,6 +232,11 @@ void cooling_shutoff(float temp)
     float target_bottom = target_temp - temp_band;
     if(temp <= target_bottom) {
         // Turn freezer pin off
+        Serial.println("Shutting off freezer");
+        Serial.print("Current temp: ");
+        Serial.println(temp);
+        Serial.print("Target temp: ");
+        Serial.println(target_bottom);
         digitalWrite(COOLING_PIN, LOW);
         cooling = false;
         cooling_off_time = millis();
